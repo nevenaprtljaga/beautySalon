98de3c7d7eda6cd7068c7a2e23781f016075fbe1
nevenaprtljaga <nevenaprtljaga@gmail.com>
2023-01-06 12:17:06 +0100

    addSalon

--- js/admin-panel.js
+++ js/admin-panel.js
@@ -3,53 +3,52 @@
 // console.log(localStorage.getItem("salons"));
 
 let data = JSON.parse(localStorage.getItem("salons"));
-var tableData = data.map(salon => (
-    `
-      <tr>
-        <td>${salon.name}</td>
-        <td>${salon.address}</td>
-        <td>${JSON.stringify(salon.treatments)}</td>
-      </tr>
-    `
-  )).join('');
 
-var tbody = document.querySelector('#body');
-tbody.innerHTML = tableData;
+let listTre = [];
+var tbody = document.getElementById('body');
+
+for (let i = 0; i < data.length; i++) {
+    let salon1 = data[i];
+    listTre = [];
+    for (let j = 0; j < salon1.treatments.length; j++) {
+        listTre += salon1.treatments[j].name + ": " + salon1.treatments[j].price + "RSD; ";
+    }
+    tr = "<tr>";
+    tr = `
+        <tr>
+          <td>${salon1.name}</td>
+          <td>${salon1.address}</td>
+          <td>${listTre}</td>
+        </tr>
+    `;
+    tbody.innerHTML += tr;
+
+}
+
 //add salon
-document.getElementById("addSalonButton").addEventListener("click", addSalon); 
+document.getElementById("addSalonButton").addEventListener("click", addSalon);
 
-function addSalon(){
+function addSalon() {
     let salonName = document.getElementById("salonNameInput").value;
     let salonAddress = document.getElementById("salonAddressInput").value;
     let salonTreatments = document.getElementById("salonTreatmentsInput").value;
     let existingSalons = JSON.parse(localStorage.getItem("salons")) || [];
-
-    
     let arrTreatments = salonTreatments.split(",");
-
-
     let newSalon = {
         name: salonName,
-        address : salonAddress,
-        treatments : []
+        address: salonAddress,
+        treatments: []
     };
-    
 
-    for(let i = 0; i < arrTreatments.length; i+=2){
-        newSalon.treatments.push({name: arrTreatments[i], price: arrTreatments[i+1]});
+    for (let i = 0; i < arrTreatments.length; i += 2) {
+        newSalon.treatments.push({ name: arrTreatments[i], price: arrTreatments[i + 1] });
     }
-    
-  
 
-    if(!existingSalons.some(salon => salon["name"] === salonName)){
+    if (!existingSalons.some(salon => salon["name"] === salonName)) {
         existingSalons.push(newSalon);
         localStorage.setItem("salons", JSON.stringify(existingSalons));
-        console.log(localStorage.getItem("salons"));
-    }else{
+        window.location.reload();
+    } else {
         alert("Salon name exists in database.");
     }
 }
-
-
-//delete usera
-// delete appointmenta
--- js/user-panel.js
+++ js/user-panel.js
@@ -4,7 +4,7 @@ let salonNames = [];
 let data = JSON.parse(localStorage.getItem("salons"));
 var count = Object.keys(data).length;
 
-for(let i = 0; i < count; i++){
+for (let i = 0; i < count; i++) {
     salonNames.push(data[i].name);
 }
 
@@ -15,39 +15,38 @@ for (let i = 0; i < salonNames.length; ++i) {
 
 //treba da cita tretmane iz baze salona
 
-//dodaj cenu pored tretmana!!!
 let treatmentNamesDrop = document.getElementById("treatment-names");
-function fillTreatments(){
+function fillTreatments() {
     let arrSalon = data.find(element => element.name == salonNamesDrop.value);
     let arrtre = arrSalon.treatments;
     for (a in treatmentNamesDrop.options) { treatmentNamesDrop.options.remove(0); }
     for (let i = 0; i < treatmentNames.length; ++i) {
-        treatmentNamesDrop[treatmentNamesDrop.length] = new Option(arrtre[i].name, arrtre[i].name);
+        treatmentNamesDrop[treatmentNamesDrop.length] = new Option(arrtre[i].name + " - " + arrtre[i].price + " RSD", arrtre[i].name + " - " + arrtre[i].price + " RSD");
     }
 }
 document.getElementById("reserveButton").addEventListener("click", makeAp);
 
 
 
-function makeAp(){
+function makeAp() {
     let date = document.getElementById("date").value;
     let time = document.getElementById("time").value;
     let existingAppo = JSON.parse(localStorage.getItem("appointments")) || [];
-    if(localStorage.getItem("currentUser") == "" || salonNamesDrop.value == "choose" || treatmentNamesDrop.value == "choose"  || date == "" || time == ""){
+    if (localStorage.getItem("currentUser") == "" || salonNamesDrop.value == "choose" || treatmentNamesDrop.value == "choose" || date == "" || time == "") {
         alert("You need to enter the information!");
-    }else{
+    } else {
         let appo = [{
-            "username" : localStorage.getItem("currentUser"),
-            "salonName" : salonNamesDrop.value,
-            "treatmentName" : treatmentNamesDrop.value,
-            "date" : date,
-            "time" : time
+            "username": localStorage.getItem("currentUser"),
+            "salonName": salonNamesDrop.value,
+            "treatmentName": treatmentNamesDrop.value,
+            "date": date,
+            "time": time
         }]
         existingAppo.push(appo);
         localStorage.setItem("appointments", JSON.stringify(existingAppo));
-        alert("Sucessful appointment!");    
+        alert("Sucessful appointment!");
         console.log(localStorage.getItem("appointments"));
-        window.location.href="../html/home.html";
+        window.location.href = "../html/home.html";
     }
 
 }
